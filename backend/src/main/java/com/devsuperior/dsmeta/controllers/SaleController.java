023d711998795558317b4d1b69662a211328e044
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2022-09-09 21:04:32 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value="/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService saleService;
+	
+	@GetMapping 
+	public ResponseEntity<List<Sale>> findSales(){
+		List<Sale> sale = saleService.findSales();
+		return ResponseEntity.status(HttpStatus.OK).body(sale);
+		
+	}
+
+}
